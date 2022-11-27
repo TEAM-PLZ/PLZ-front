0ef02fc087243a390e91437cde5df5aa5912916d
highjoon <yundosa2@gmail.com>
2022-11-28 03:59:28 +0900

    feat[write]: 메시지 작성 기능 구현

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -4,7 +4,7 @@ import styles from './main.module.css';
 const Main = () => {
   return (
     <div className={styles.container}>
-      <Header/>
+      <Header page="home" />
       <p className={styles.headline + 'heading2'}>
         <span>새로운 플리가 도착했어요</span>
         <br />
--- src/pages/write/index.tsx
+++ src/pages/write/index.tsx
@@ -5,6 +5,10 @@ import useInputs from 'hooks/useInputs';
 import { IYoutube } from 'types';
 import setThumbnailFileName from 'utils/setThumbnailFileName';
 import { sendAlbum } from 'services/album';
+import Header from 'components/Header';
+import styles from './write.module.css';
+import SnackBar from 'components/SnackBar';
+import getRandomImage from 'utils/getRandomImage';
 
 interface IForm {
   singer: string;
@@ -13,11 +17,22 @@ interface IForm {
   message: string;
 }
 
-const WriteTest = () => {
+interface ISnackBarList {
+  status: string;
+  message: string;
+}
+
+const Write = () => {
   const [url, setUrl] = useState<string>('');
   const [thumbnailUrl, setThumbnailUrl] = useState<string>('');
   const [coverImgUrl, setCoverImgUrl] = useState<string>('');
   const [coverImgFile, setCoverImgFile] = useState<File>();
+  const [randomImageSrc, setRandomImageSrc] = useState<string>('/images/image1.png');
+
+  const [snackBar, setSnackBar] = useState<ISnackBarList>({
+    status: '',
+    message: '',
+  });
   const [{ singer, title, toNickname, message }, handleChange] = useInputs<IForm>({
     singer: '',
     title: '',
@@ -30,9 +45,9 @@ const WriteTest = () => {
     try {
       const result = await axios.get<IYoutube>(`https://www.youtube.com/oembed?url=${url}`);
       setThumbnailUrl(setThumbnailFileName(result.data.thumbnail_url));
+      setSnackBar({ status: 'success', message: '성공적으로 링크를 가져왔습니다.' });
     } catch (e) {
-      // 에러 스낵바 생성
-      console.log(e);
+      setSnackBar({ status: 'failure', message: '링크를 다시 확인해주세요.' });
     }
   };
 
@@ -43,8 +58,16 @@ const WriteTest = () => {
   const onCoverImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) return;
     if (coverImgUrl) URL.revokeObjectURL(coverImgUrl);
+
     setCoverImgFile(e.target.files[0]);
+    setRandomImageSrc('');
     setCoverImgUrl(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const onRandomImageClick = () => {
+    setRandomImageSrc(getRandomImage(randomImageSrc));
+    setCoverImgUrl('');
+    setCoverImgFile(undefined);
   };
 
   const handleSubmitYoutubeLink = (e: React.FormEvent<HTMLFormElement>) => {
@@ -57,11 +80,13 @@ const WriteTest = () => {
     const newFormData = new FormData();
 
     const obj = {
-      url,
-      thumbnailUrl,
-      singer,
-      title,
       message,
+      randomCoverPath: randomImageSrc,
+      receiverId: '3',
+      singer,
+      thumbnailImgPath: thumbnailUrl,
+      title,
+      url,
       writerNickname: toNickname,
     };
 
@@ -69,55 +94,85 @@ const WriteTest = () => {
       newFormData.append(`${key}`, value);
     });
 
-    newFormData.append('imgFile', coverImgFile as File);
+    if (coverImgFile) newFormData.append('coverImgFile', coverImgFile as File);
 
     sendAlbum(newFormData)
-      .then(res => console.log(res))
+      .then(res => {
+        console.log(res);
+        URL.revokeObjectURL(coverImgUrl);
+      })
       .catch(e => console.log(e));
   };
 
   return (
-    <section>
-      {thumbnailUrl && <Image src={thumbnailUrl} width="200" height="113" alt="썸네일" />}
-      {coverImgUrl && <Image src={coverImgUrl} width="200" height="113" alt="커버" />}
-      <form onSubmit={handleSubmitYoutubeLink}>
-        <label>유튜브 링크 입력</label>
-        <input type="url" onChange={onUrlChange} value={url} />
-        <button type="submit">링크 제출</button>
-      </form>
-      <hr />
-      <div>
-        <button>
-          <label htmlFor="input-image">커버 업로드</label>
-        </button>
-        <input
-          type="file"
-          onChange={onCoverImageChange}
-          name="image"
-          id="input-image"
-          accept="image/*"
-          style={{ display: 'none' }}
-        />
-      </div>
-      <form className="flex flex-col" onSubmit={handleSubmitFormData}>
-        <p>가수이름 </p>
-        <input type="text" name="singer" onChange={handleChange} value={singer} />
-        <p>노래 제목 </p>
-        <input type="text" name="title" onChange={handleChange} value={title} />
-        <p>닉네임 </p>
-        <input type="text" name="toNickname" onChange={handleChange} value={toNickname} />
-        <p>메시지 </p>
-        <input type="text" name="message" onChange={handleChange} value={message} />
+    <>
+      <div className={styles.container}>
+        <Header page="write" />
+        <h1 className={`heading1`}>LP에 담을 곡 선택</h1>
+        <form onSubmit={handleSubmitYoutubeLink}>
+          <input
+            type="url"
+            onChange={onUrlChange}
+            value={url}
+            placeholder="내 마음을 전할 음악을 선택해주세요"
+            className={`body2 ${styles.input_url}`}
+          />
+          <button type="submit" className={`body1 ${styles.button_url}`}>
+            링크 가져오기
+            <Image
+              src={'/icons/youtube.svg'}
+              width="28"
+              height="28"
+              alt="youtube"
+              className="ml-[6px]"
+            />
+          </button>
+        </form>
         <div>
-          <button onClick={() => console.log(singer, title, toNickname, message)}>콘솔 확인</button>
-          <button type="submit">제출하기</button>
+          <button className="bg-black">
+            <label htmlFor="input-image">커버 업로드</label>
+          </button>
+          <input
+            type="file"
+            onChange={onCoverImageChange}
+            name="image"
+            id="input-image"
+            accept="image/*"
+            className="hidden"
+          />
         </div>
-      </form>
-    </section>
+        <div>
+          <div className="relative w-[300px] h-[300px]">
+            <Image
+              src={coverImgUrl ? coverImgUrl : randomImageSrc}
+              fill
+              sizes="300px"
+              alt="random_image"
+              style={{ objectFit: 'cover' }}
+            />
+          </div>
+          <button onClick={onRandomImageClick}>새로고침</button>
+        </div>
+        <form className="flex flex-col" onSubmit={handleSubmitFormData}>
+          <p>가수이름 </p>
+          <input type="text" name="singer" onChange={handleChange} value={singer} />
+          <p>노래 제목 </p>
+          <input type="text" name="title" onChange={handleChange} value={title} />
+          <p>닉네임 </p>
+          <input type="text" name="toNickname" onChange={handleChange} value={toNickname} />
+          <p>메시지 </p>
+          <input type="text" name="message" onChange={handleChange} value={message} />
+          <div>
+            <button type="submit">제출하기</button>
+          </div>
+        </form>
+      </div>
+      {snackBar?.status !== '' && <SnackBar snackBar={snackBar} setSnackBar={setSnackBar} />}
+    </>
   );
 };
 
-export default WriteTest;
+export default Write;
 
 //https://youtu.be/
 //https://www.youtube.com/watch?v=
