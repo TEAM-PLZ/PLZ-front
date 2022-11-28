58d357e06b7fd23284006f203726c28bb2be3468
Sangwon Park <wkqkel@naver.com>
2022-11-29 04:12:18 +0900

    feat[login,헤더]: 로그아웃 구현

---
+++ src/components/Header/LogoutButton.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { deleteCookie } from 'cookies-next';
+import { logoutKaKao } from 'services/auth';
+import { TPage } from '.';
+
+interface IProps {
+  page: TPage;
+}
+
+const LogoutButton = ({ page }: IProps) => {
+  const router = useRouter();
+
+  const onClick = async () => {
+    try {
+      await logoutKaKao();
+      deleteCookie('token');
+      router.push('/');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <button onClick={onClick}>
+      <Image
+        src={page === 'home' ? '/icons/logout.svg' : '/icons/white_logout.svg'}
+        width="24"
+        height="24"
+        alt="logout_icon"
+      />
+    </button>
+  );
+};
+
+export default LogoutButton;
