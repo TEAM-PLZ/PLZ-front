8482c9c4b1678076b7babe1089a31719a454e021
Sangwon Park <wkqkel@naver.com>
2022-11-28 22:11:29 +0900

    Delete src/components/snackBarButton directory

--- src/components/snackBarButton/index.tsx
+++
@@ -1,5 +0,0 @@
-import Styles from './SnackBarButton.module.css';
-
-const Button = ({children, handleClick}: any) => <button className={Styles.buttons} onClick={handleClick}>{children}</button>
-
-export default Button
