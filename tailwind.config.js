3165732345bcf49a670dc3e32842a3c920531849
highjoon <yundosa2@gmail.com>
2022-11-28 20:44:10 +0900

    design[detail]: 메시지 디자인 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  
+
   theme: {
     extend: {
       colors: {
-        'snackbar': '#1B2125'
-      }
+        snackbar: '#1B2125',
+      },
+      backgroundImage: {
+        messageBackground: 'url("/images/message_background.png")',
+      },
     },
   },
   plugins: [],
