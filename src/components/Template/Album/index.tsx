e604904911bee7c65bddbf708d6bee843e6036f2
highjoon <yundosa2@gmail.com>
2022-11-29 15:26:40 +0900

    feat[main]: 앨범 리스트 api 연결, 클릭 시 디테일 페이지 연결, 복사 주소 설정, 그 외 디자인 수정

--- src/components/Template/Album/index.tsx
+++ src/components/Template/Album/index.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable no-nested-ternary */
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import React from 'react';
 import { IAlbum } from 'types/index';
 import styles from './album.module.css';
@@ -6,17 +8,31 @@ import styles from './album.module.css';
 interface IProps {
   album: IAlbum;
 }
-const IMAGE_PROPS = { width: 120, height: 120, className: 'w-full h-auto' };
+const IMAGE_PROPS = { width: 120, height: 120, className: 'w-full h-full object-cover' };
 
 const Album = ({ album }: IProps) => {
-  const newIconStyle = !album?.read ? { opacity: 0 } : {};
+  const router = useRouter();
+  // const newIconStyle = !album?.read ? { opacity: 0 } : {};
   const hasImage = album?.coverImgPath;
+
+  const moveToSongDetail = () => {
+    if (album === undefined) return;
+    router.push(`/song/${album.id}`);
+  };
+
   return (
     <div className={styles.album}>
-      <div className={styles.newIcon} style={newIconStyle}>
-        NEW
-      </div>
-      <div className={styles.albumBox}>
+      {album?.read === false && (
+        <div className={styles.newIcon}>
+          {/* <div className={styles.newIcon} style={newIconStyle}> */}
+          NEW
+        </div>
+      )}
+      <div
+        className={`${styles.albumBox} ${album !== undefined ? 'cursor-pointer' : ''}`}
+        role="presentation"
+        onClick={moveToSongDetail}
+      >
         {hasImage && (
           <Image
             src="/images/album_backdrop.svg"
@@ -26,7 +42,17 @@ const Album = ({ album }: IProps) => {
             height={114}
           />
         )}
-        <Image src={album?.coverImgPath || '/icons/empty_lp.svg'} alt="lp" {...IMAGE_PROPS} />
+        <Image
+          src={
+            album?.coverImgPath
+              ? `${process.env.NEXT_PUBLIC_BASE_URL}/${album?.coverImgPath}`
+              : album?.randomCoverPath
+              ? album?.randomCoverPath
+              : '/icons/empty_lp.svg'
+          }
+          alt="lp"
+          {...IMAGE_PROPS}
+        />
         {hasImage && <Image src="/images/album_light.svg" alt="lp" {...IMAGE_PROPS} />}
       </div>
     </div>
