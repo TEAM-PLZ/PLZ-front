745d9213b562f1f7e418e32b4b0b012bcab64523
seoyoonyi <yiseoyoon@gmail.com>
2022-11-29 21:32:15 +0900

    feat[kak 페이지 연결(온보딩, 메인)

--- src/pages/kakaoLogin/index.tsx
+++ src/pages/kakaoLogin/index.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { checkUser } from 'services/auth';
 import { setCookie } from 'cookies-next';
 import type { IUserInfo } from 'types/login';
+import { useRouter } from 'next/router';
 
 interface IProps {
   token: string;
@@ -11,13 +12,16 @@ interface IProps {
 const COOKIE_OPTIONS = { path: '/', maxAge: 28800 }; // 8H
 
 const KakaoAuth: NextPage<IProps> = ({ token }: IProps) => {
+  const router = useRouter();
   const getToken = async () => {
     setCookie('token', token, COOKIE_OPTIONS);
     const result: IUserInfo = await checkUser<IUserInfo>();
     if (result?.isFirst) {
       localStorage.setItem('isFirst', String(result.isFirst));
+      router.push('/onBoarding');
     } else {
       localStorage.removeItem('isFirst');
+      router.push('/main');
     }
     window.self.close();
   };
